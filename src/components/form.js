5d6c0f28f0fc6f2ded91c42b15b80635af810c8f
Vadim Constantin <vad.constantin@gmail.com>
2022-05-17 01:21:07 +0200

    added form to main page

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,45 @@
 import React, { useState} from 'react'
+import './form.css'
 
 const Form = () => {
+
+  const [ profile, setProfile ] = useState({})
+
+  const handleChange = ({target}) => {
+    const { name, value } = target
+    setProfile((prev) => {
+      return {...prev, [name]: value}
+    })
+  }
+
   return(
-    <h1>Hello</h1>
+    <div>
+      <form className="form">
+        <input
+        onChange={handleChange}
+        value={profile.firstName || ''}
+        type="text"
+        placeholder="First Name"
+        name="firstName"
+        />
+        <input
+          onChange={handleChange}
+          value={profile.surName || ''}
+          type="text"
+          placeholder="Surname"
+          name="surName"
+        />
+        <input
+          onChange={handleChange}
+          value={profile.age || ''}
+          type="number"
+          placeholder="Age"
+          name="age"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Profil : {profile.firstName} {profile.surName}, you are {profile.age}</p>
+    </div>
   )
 
 }
