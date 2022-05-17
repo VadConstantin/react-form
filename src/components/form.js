f56c8bd1a3da8b6972a36edd7aaba36562564b34
Vadim Constantin <vad.constantin@gmail.com>
2022-05-17 13:25:55 +0200

    added bootstrap style to form

--- src/components/form.js
+++ src/components/form.js
@@ -7,38 +7,54 @@ const Form = () => {
 
   const handleChange = ({target}) => {
     const { name, value } = target
+    console.log(name)
+
     setProfile((prev) => {
       return {...prev, [name]: value}
     })
   }
 
   return(
-    <div>
-      <form className="form">
-        <input
-        onChange={handleChange}
-        value={profile.firstName || ''}
-        type="text"
-        placeholder="First Name"
-        name="firstName"
-        />
-        <input
-          onChange={handleChange}
-          value={profile.surName || ''}
-          type="text"
-          placeholder="Surname"
-          name="surName"
-        />
-        <input
-          onChange={handleChange}
-          value={profile.age || ''}
-          type="number"
-          placeholder="Age"
-          name="age"
-        />
-        <button type="submit">Submit</button>
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="name"> First Name: </label>
+          <input className="form-control"
+            onChange={handleChange}
+            value={profile.firstName || ''}
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            id="name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="surName"> Family Name: </label>
+          <input className="form-control"
+            onChange={handleChange}
+            value={profile.surName || ''}
+            type="text"
+            placeholder="Surname"
+            name="surName"
+            id="surName"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="Age"> Age: </label>
+          <input className="form-control"
+            onChange={handleChange}
+            value={profile.age || ''}
+            type="number"
+            placeholder="Age"
+            name="age"
+            id="Age"
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </div>
       </form>
-      <p>Profil : {profile.firstName} {profile.surName}, you are {profile.age}</p>
+      <p>Profil : {profile.firstName} {profile.surName} {profile.age}</p>
     </div>
   )
 
